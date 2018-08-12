6f55d37e2eea8b4800018c0883dd6c2a176ab9cc
Josh Mcguigan <joshmcg88@gmail.com>
2018-08-12 14:55:15 -0700

    Improvements for blog post

--- src/flexible.rs
+++ src/flexible.rs
@@ -15,12 +15,29 @@ fn fizzbuzz<'a, T>(nums: T)
     }
 }
 
+fn fizzbuzz_vec(nums: &Vec<usize>) {
+    for num in nums {
+        let divisible_by_three = num % 3 == 0;
+        let divisible_by_five = num % 5 == 0;
+
+        match (divisible_by_three, divisible_by_five) {
+            (true, true) => println!("FizzBuzz"),
+            (true, false) => println!("Fizz"),
+            (false, true) => println!("Buzz"),
+            (false, false) => println!("{}", num),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works(){
+        let nums : Vec<usize> = vec![1,2,3,4,5,15];
+        fizzbuzz_vec(&nums);
+
         let nums : Vec<usize> = vec![1,2,3,4,5,15];
         fizzbuzz(&nums);
 
